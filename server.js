da57e427c367bcd5fba18422583ff7615139a3c6
Lmiguel <32590888+Malcol@users.noreply.github.com>
2020-07-25 17:52:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ io.on('connection', function(socket) {
     socket.emit('update-users', users);
 
     socket.on('send-msg', function(data) {
-        var newMessage = { text : data.message, user : data.user, date : dateFormat(new Date(), 'shortTime') };
+        var newMessage = { text : data.message, user : data.user, date : dateFormat(new Date(), 'longTime') };
         messages.push(newMessage);
         io.emit('read-msg', newMessage);
     });
